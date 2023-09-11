347813d4cdf44053772206a40916a0804bb70f1c
AmoabaKelvin <kel.amoaba@gmail.com>
2023-09-11 22:59:33 +0000

    add validate input and copy ToClipboard functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,19 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import * as z from "zod";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+const urlInputSchema = z.object({
+  url: z.string().url(),
+});
+
+export function validateUrlInput(input: unknown): input is { url: string } {
+  return urlInputSchema.safeParse(input).success;
+}
+
+export const copyToClipboard = (text: string) => {
+  navigator.clipboard.writeText(text);
+};
