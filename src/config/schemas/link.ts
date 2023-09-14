c44f836cdc3ab196c73e04927969015d23910e6f
AmoabaKelvin <kel.amoaba@gmail.com>
2023-09-14 15:07:56 +0000

    remove `min` on alias field

--- src/config/schemas/link.ts
+++ src/config/schemas/link.ts
@@ -2,7 +2,7 @@ import * as zod from "zod";
 
 export const linkSchema = zod.object({
   url: zod.string().url(),
-  alias: zod.string().min(3).max(10).optional(),
+  alias: zod.string().max(10).optional(),
 });
 
 export type Link = zod.infer<typeof linkSchema>;
