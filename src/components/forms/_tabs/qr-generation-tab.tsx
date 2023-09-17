fdc5e1f6e4e7f1a7ee37598a3b4d7ca9bde3432a
AmoabaKelvin <kel.amoaba@gmail.com>
2023-09-17 10:19:38 +0000

    ✨ qrcode generation

--- src/components/forms/_tabs/qr-generation-tab.tsx
+++ src/components/forms/_tabs/qr-generation-tab.tsx
@@ -9,32 +9,106 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@radix-ui/react-label";
-import React from "react";
+import React, { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import QRCode from "qrcode.react";
+import { motion, AnimatePresence } from "framer-motion";
+import { cn } from "@/lib/utils";
+
+interface UrlShortnerInputs {
+  content: string;
+}
 
 const QRGenerationTab = () => {
+  const [enteredContent, setenteredContent] = useState("");
+  const qrCodeCanvasRef = React.useRef(null);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UrlShortnerInputs>();
+
+  const onSubmit: SubmitHandler<UrlShortnerInputs> = async (data) => {
+    setenteredContent(data.content);
+  };
+
+  const downloadQRCode = () => {
+    if (!enteredContent) return;
+    const canvas = document.getElementById("qr-gen") as HTMLCanvasElement;
+    const pngUrl = canvas!
+      .toDataURL("image/png")
+      .replace("image/png", "image/octet-stream");
+    let downloadLink = document.createElement("a");
+    downloadLink.href = pngUrl;
+    downloadLink.download = `qrcode.png`;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  };
+
   return (
     <Card>
-      <CardHeader>
-        <CardTitle>Generate QR Code</CardTitle>
-        <CardDescription>
-          Generate beautiful QR codes for your brand.
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="space-y-2">
-        <div className="space-y-1">
-          <Label htmlFor="current">Link or Text</Label>
-          <Input
-            id="current"
-            type="text"
-            placeholder="tosomebeautifulpage.com"
-          />
-        </div>
-      </CardContent>
-      <CardFooter>
-        <Button className="text-sm text-white bg-black hover:text-green-600 hover:bg-green-50 active:scale-95 active:ring-4 active:ring-green-300 transition-transform duration-300 ease-in-out">
-          Generate
-        </Button>
-      </CardFooter>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <CardHeader>
+          <CardTitle>Generate QR Code</CardTitle>
+          <CardDescription>
+            Generate beautiful QR codes for your brand.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-2">
+          <div className="space-y-1">
+            <Label htmlFor="current">Link or Text</Label>
+            <Input
+              id="current"
+              type="text"
+              placeholder="tosomebeautifulpage.com"
+              {...register("content", {
+                required: true,
+              })}
+            />
+          </div>
+        </CardContent>
+        <CardFooter className={cn(enteredContent && "flex-col gap-2.5")}>
+          <AnimatePresence mode="wait">
+            {enteredContent && (
+              <motion.div
+                key="qr-code"
+                initial={{ opacity: 0, y: -20 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -20 }}
+                transition={{ duration: 0.7 }}
+              >
+                <QRCode
+                  id="qr-gen"
+                  value={enteredContent}
+                  size={300}
+                  level={"H"}
+                  includeMargin={true}
+                  ref={qrCodeCanvasRef}
+                />
+              </motion.div>
+            )}
+          </AnimatePresence>
+          <AnimatePresence mode="wait">
+            <motion.div
+              key="download-button"
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 20 }}
+              transition={{ duration: 0.7 }}
+            >
+              <Button
+                className="text-sm text-white bg-black duration-300 hover:animate-out"
+                type="submit"
+                onClick={downloadQRCode}
+              >
+                {enteredContent ? "Download QR Code" : "Generate QR Code"}{" "}
+              </Button>
+            </motion.div>
+          </AnimatePresence>
+        </CardFooter>
+      </form>
     </Card>
   );
 };
