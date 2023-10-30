4694c8caf0b50ffac03fb2dfaa21ae91d7a3b747
AmoabaKelvin <kel.amoaba@gmail.com>
2023-10-30 09:16:50 +0000

    🎨

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,28 +19,28 @@ export default function Home() {
   return (
     <>
       <LandingPageNav />
-      <main className="flex min-h-screen flex-col items-center p-4 md:p-28">
-        <section className="flex flex-col gap-6 md:flex-row w-full">
+      <main className="flex flex-col items-center min-h-screen p-4 md:p-28">
+        <section className="flex flex-col w-full gap-6 md:flex-row">
           {/* Left side */}
-          <div className="flex flex-col mt-16 md:-mt-5  justify-center items-center lg:items-start lg:justify-start lg:w-2/3">
-            <h1 className=" font-bold text-center lg:text-left font-ocean leading-10">
-              <span className="leading-10 text-5xl md:text-6xl ">
+          <div className="flex flex-col items-center justify-center mt-16 md:-mt-5 lg:items-start lg:justify-start lg:w-2/3">
+            <h1 className="font-bold leading-10 text-center lg:text-left font-ocean">
+              <span className="text-5xl leading-10 md:text-6xl ">
                 Transform links{" "}
               </span>
               <br />
-              <span className="text-green-500 text-4xl md:text-5xl font-ocean">
+              <span className="text-4xl text-green-500 md:text-5xl font-ocean">
                 unleash results
               </span>
             </h1>
 
-            <p className="text-center lg:text-left text-gray-100 mt-5 font-mazzardRegular leading-7">
+            <p className="mt-5 leading-7 text-center text-gray-100 lg:text-left font-mazzardRegular">
               Power up your links with our AI-driven analytics, advanced URL
               shortening, and dynamic QR code creation and boost engagement
               results like never before. Unleash the power of your links today!
             </p>
 
             <button
-              className="bg-green-500 text-white px-8 py-2 rounded-md mt-8 font-mazzardRegular hover:bg-green-600 duration-300"
+              className="px-8 py-2 mt-8 text-white duration-300 bg-green-500 rounded-md font-mazzardRegular hover:bg-green-600"
               onClick={handleTryNowClick}
             >
               Try now
@@ -48,7 +48,7 @@ export default function Home() {
           </div>
 
           {/* Right side */}
-          <div className="hidden lg:flex justify-center -mt-5 items-center md:w-1/2">
+          <div className="items-center justify-center hidden -mt-5 lg:flex md:w-1/2">
             <Image
               src={landingImage}
               alt="illustration"
@@ -59,7 +59,7 @@ export default function Home() {
         </section>
 
         {/* section for the try out section, displays a tab where you can switch between generating QR Codes and shortnening links */}
-        <section className=" w-11/12 flex justify-center my-16">
+        <section className="flex justify-center w-11/12 my-16 ">
           <div ref={tryOutRef}>
             {" "}
             <LinkShortenerAndQRGenerator />{" "}
@@ -67,7 +67,7 @@ export default function Home() {
         </section>
 
         <section className="md:w-6/12">
-          <h1 className="text-2xl font-mazzard font-bold text-center my-10">
+          <h1 className="my-10 text-2xl font-bold text-center font-mazzard">
             Frequently Asked Questions
           </h1>
           <FrequentlyAskedQuestions />
