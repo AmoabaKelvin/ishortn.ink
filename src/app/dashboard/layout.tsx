8c520314f5f665ed34ea650c3a50f837fd152fb4
AmoabaKelvin <kel.amoaba@gmail.com>
2023-11-13 10:51:39 +0000

    Refactor dashboard layout to use NavigationBar component

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+import NavigationBar from "@/components/dashboard/navigation-bar";
 
 import { satoshi } from "@/styles/fonts";
 
@@ -7,12 +7,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
     <div
       className={`px-4 py-10 mx-auto text-black max-w-7xl sm:px-6 lg:px-8 ${satoshi.className}`}
     >
-      <div className="flex items-center justify-between">
-        <h2 className="text-xl font-semibold leading-tight text-gray-800">
-          <span className="text-blue-600">ishortn.ink</span> / Dashboard
-        </h2>
-        <Button className="w-10 h-10 rounded-full">KA</Button>
-      </div>
+      <NavigationBar />
 
       <div className="py-4 mt-4">{children}</div>
     </div>
