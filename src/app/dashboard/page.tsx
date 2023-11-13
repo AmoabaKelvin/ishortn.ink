27456b486307f125f52980cef84b66c9e7bbd0c3
AmoabaKelvin <kel.amoaba@gmail.com>
2023-11-13 10:51:47 +0000

    Add TabSwitcher component and remove Link components

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,42 +1,13 @@
+import LinkShowcase from "@/components/dashboard/link-showcase";
+import TabSwitcher from "@/components/dashboard/tab-switcher";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import Link from "next/link";
-import LinkShowcase from "./_components/link-showcase";
 
 const Dashboard = () => {
   return (
     <main className="flex flex-col gap-10">
-      <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
-        <ul className="flex flex-wrap -mb-px">
-          <li className="me-2">
-            <Link
-              href="#"
-              className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-            >
-              Profile
-            </Link>
-          </li>
-          <li className="me-2">
-            <Link
-              href="#"
-              className="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500"
-              aria-current="page"
-            >
-              Links
-            </Link>
-          </li>
-          <li className="me-2">
-            <Link
-              href="#"
-              className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-            >
-              Settings
-            </Link>
-          </li>
-        </ul>
-      </div>
-
+      <TabSwitcher />
       <div className="flex items-center justify-between">
         <h2 className="text-xl font-semibold leading-tight text-gray-800">
           Links
