b5279f692a51935c23aed1e3deeb710209b161a1
AmoabaKelvin <kel.amoaba@gmail.com>
2023-11-17 12:43:15 +0000

    Remove TabSwitcher component from UserProfilePage

--- src/app/dashboard/profile/[[...user-profile]]/page.tsx
+++ src/app/dashboard/profile/[[...user-profile]]/page.tsx
@@ -1,12 +1,8 @@
-import TabSwitcher from "@/components/dashboard/tab-switcher";
 import { UserProfile } from "@clerk/nextjs";
 
 const UserProfilePage = () => (
-  <div className="flex flex-col gap-10">
-    <TabSwitcher />
-    <div className="flex flex-col items-center justify-between w-full">
-      <UserProfile path="/dashboard/profile" routing="path" />
-    </div>
+  <div className="flex flex-col items-center justify-between w-full">
+    <UserProfile path="/dashboard/profile" routing="path" />
   </div>
 );
 
