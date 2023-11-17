1825c290a5e12609e64dfd5b88a367a56dbb422a
AmoabaKelvin <kel.amoaba@gmail.com>
2023-11-17 12:43:02 +0000

    Add LinkCreationPage component

---
+++ src/app/dashboard/links/page.tsx
@@ -0,0 +1,11 @@
+import LinkEditForm from "@/components/forms/dashboard/links/link-edit-form";
+
+const LinkCreationPage = () => {
+  return (
+    <section>
+      <LinkEditForm />
+    </section>
+  );
+};
+
+export default LinkCreationPage;
