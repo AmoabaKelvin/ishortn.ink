6d3aa373186368110a15d7f32b493963576ca56a
AmoabaKelvin <kel.amoaba@gmail.com>
2023-11-17 12:43:22 +0000

    Add "Create Link" tab to dashboard

--- src/components/dashboard/tab-switcher.tsx
+++ src/components/dashboard/tab-switcher.tsx
@@ -8,6 +8,7 @@ import { cn } from "@/lib/utils";
 const links = [
   { name: "Links", href: "/dashboard" },
   { name: "Profile", href: "/dashboard/profile" },
+  { name: "Create Link", href: "/dashboard/links" },
 ];
 
 const TabSwitcher = () => {
