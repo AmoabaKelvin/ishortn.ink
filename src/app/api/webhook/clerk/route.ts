13e5b0e091ce94bf1b0f14cc83b0846aa1b9c813
AmoabaKelvin <kel.amoaba@gmail.com>
2023-11-17 08:08:13 +0000

    improve formatting

--- src/app/api/webhook/clerk/route.ts
+++ src/app/api/webhook/clerk/route.ts
@@ -10,7 +10,7 @@ export async function POST(req: Request) {
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
-      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
+      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local",
     );
   }
 
