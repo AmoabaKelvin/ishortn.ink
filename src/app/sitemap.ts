304635a9db24f98b82ea7afb267244bbb50310b2
AmoabaKelvin <kel.amoaba@gmail.com>
2023-11-17 08:30:41 +0000

    Add new pages to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,10 +1,21 @@
 import { MetadataRoute } from "next";
 
+const pages = [
+  "dashboard",
+  "dashboard/analytics",
+  "dashboard/links",
+  "dashboard/profile",
+];
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://www.ishortn.ink",
       lastModified: new Date(),
     },
+    ...pages.map((page) => ({
+      url: `https://www.ishortn.ink/${page}`,
+      lastModified: new Date(),
+    })),
   ];
 }
