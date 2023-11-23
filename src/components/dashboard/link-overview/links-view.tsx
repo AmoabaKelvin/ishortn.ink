a5555db873ad3b6b2d9a5d2eb6700eca8674c604
AmoabaKelvin <kel.amoaba@gmail.com>
2023-11-23 23:57:38 +0000

    enable link filtering

---
+++ src/components/dashboard/link-overview/links-view.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useState } from "react";
+
+import React from "react";
+
+import { Prisma } from "@prisma/client";
+import { Input } from "@/components/ui/input";
+import LinkShowcase from "./link-showcase";
+
+type Link = Prisma.LinkGetPayload<{
+  include: {
+    linkVisits: true;
+  };
+}>;
+
+const LinksView = ({ links }: { links: Link[] }) => {
+  const [search, setSearch] = useState("");
+
+  const filteredLinks = links.filter((link) =>
+    link.url.toLowerCase().includes(search.toLowerCase()),
+  );
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  return (
+    <main>
+      <Input
+        type="text"
+        placeholder="Search for a link"
+        value={search}
+        onChange={(e) => handleSearch(e)}
+      />
+      <div className="flex flex-col gap-5 mt-6">
+        {filteredLinks.map((link) => (
+          <LinkShowcase key={link.id} link={link} />
+        ))}
+      </div>
+    </main>
+  );
+};
+
+export default LinksView;
