07a820aa238dc7ad2fd9cf1fb031f8a6799ab689
AmoabaKelvin <kel.amoaba@gmail.com>
2023-11-24 11:35:23 +0000

    Add QR Code modal component

---
+++ src/components/dashboard/modals/qr-code-modal.tsx
@@ -0,0 +1,64 @@
+import { useRef } from "react";
+
+import QRCode from "qrcode.react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+type QRModalProps = {
+  destinationUrl: string;
+  open: boolean;
+  setOpen: (open: boolean) => void;
+};
+
+export function QRCodeModal({ destinationUrl, open, setOpen }: QRModalProps) {
+  const qrCodeCanvasRef = useRef(null);
+
+  const handleQRCodeDownload = () => {
+    if (!destinationUrl) return;
+    const canvas = document.getElementById("qr-gen") as HTMLCanvasElement;
+    const pngUrl = canvas!
+      .toDataURL("image/png")
+      .replace("image/png", "image/octet-stream");
+    let downloadLink = document.createElement("a");
+    downloadLink.href = pngUrl;
+    downloadLink.download = `qrcode.png`;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>QR Code</DialogTitle>
+          <DialogDescription>
+            Here is your QR Code for the link.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="flex flex-col items-center">
+          <QRCode
+            id="qr-gen"
+            value={destinationUrl}
+            size={300}
+            level={"H"}
+            includeMargin={true}
+            ref={qrCodeCanvasRef}
+          />
+
+          {/* Deactivate after Number of clicks */}
+          <Button className="mt-4" onClick={handleQRCodeDownload}>
+            Download Image
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
