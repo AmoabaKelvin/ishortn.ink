f8f48250655d2ff02a3c9da674d6b0975b4b237e
AmoabaKelvin <kel.amoaba@gmail.com>
2023-11-24 06:54:21 +0000

    Add link deletion functionality to LinkShowcase component

--- src/components/dashboard/link-overview/link-showcase.tsx
+++ src/components/dashboard/link-overview/link-showcase.tsx
@@ -9,6 +9,8 @@ import { useRouter } from "next/navigation";
 
 import { Prisma } from "@prisma/client";
 
+import { deleteLink } from "@/app/dashboard/_actions/link-actions";
+
 type Link = Prisma.LinkGetPayload<{
   include: {
     linkVisits: true;
@@ -23,6 +25,23 @@ const LinkShowcase = ({ link }: { link: Link }) => {
     (new Date().getTime() - new Date(link.createdAt).getTime()) /
       (1000 * 60 * 60 * 24),
   );
+
+  const handleLinkDeletion = async () => {
+    const response = await deleteLink(link.id);
+    console.log(response);
+    if (response && "id" in response) {
+      toast({
+        title: "Link deleted",
+        description: "Your link has been deleted.",
+      });
+    } else {
+      toast({
+        title: "Error",
+        description: "An error occurred while deleting your link.",
+        variant: "destructive",
+      });
+    }
+  };
 
   return (
     <div className="flex items-center justify-between px-6 py-4 rounded-md bg-slate-50">
@@ -70,7 +89,7 @@ const LinkShowcase = ({ link }: { link: Link }) => {
           <span className="inline md:hidden">v</span>
         </Badge>
 
-        <LinkActions />
+        <LinkActions handleDelete={handleLinkDeletion} />
       </div>
     </div>
   );
