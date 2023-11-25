2459871c42a7e4265fd1fdbf3752f28170f4e932
AmoabaKelvin <kel.amoaba@gmail.com>
2023-11-25 18:06:30 +0000

    🤞 fixes clerk not redirecting

--- src/components/dashboard/navigation-bar.tsx
+++ src/components/dashboard/navigation-bar.tsx
@@ -10,7 +10,7 @@ const NavigationBar = () => {
         </Link>{" "}
         / Dashboard
       </h2>
-      <UserButton afterSignOutUrl="/" />
+      <UserButton afterSignOutUrl="https://www.ishortn.ink/" />
     </div>
   );
 };
