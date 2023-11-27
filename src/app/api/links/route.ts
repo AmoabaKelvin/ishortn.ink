9b05067d92d17395291d3e6b8d698bc0f3807740
AmoabaKelvin <kel.amoaba@gmail.com>
2023-11-27 13:03:25 +0000

    add link disabling functionality

--- src/app/api/links/route.ts
+++ src/app/api/links/route.ts
@@ -1,9 +1,8 @@
-import * as Queries from "./queries";
 import { UAParser } from "ua-parser-js";
+import * as Queries from "./queries";
 
 import prisma from "@/db";
 import { someKnownDesktopDevices } from "../utils";
-import { headers } from "next/headers";
 
 const BASE_URL = "ishortn.ink";
 
@@ -23,12 +22,8 @@ export async function POST(req: Request) {
   return new Response(JSON.stringify({ url: `${BASE_URL}/${newLink}` }));
 }
 
-export async function GET(req: Request) {
-  console.log(">>> URL", req.url);
-  const alias = new URL(req.url).searchParams.get("alias");
-
-  // Ip address
-  const ip =
+function getUserIP(req: Request) {
+  return (
     req.headers.get("x-forwarded-for") ||
     req.headers.get("cf-connecting-ip") ||
     req.headers.get("x-real-ip") ||
@@ -36,11 +31,13 @@ export async function GET(req: Request) {
     req.headers.get("x-forwarded") ||
     req.headers.get("forwarded-for") ||
     req.headers.get("forwarded") ||
-    req.headers.get("remote-addr");
+    req.headers.get("remote-addr")
+  );
+}
 
-  console.log(">>> IP", ip);
-
-  // Log the referer
+export async function GET(req: Request) {
+  const alias = new URL(req.url).searchParams.get("alias");
+  const ip = getUserIP(req);
 
   if (!alias) {
     return new Response("Invalid URL", { status: 400 });
@@ -52,16 +49,11 @@ export async function GET(req: Request) {
     },
   });
 
-  console.log(">>> Retrieved link", retrievedLink);
-
-  if (!retrievedLink) {
+  if (!retrievedLink || retrievedLink.disabled) {
     return new Response("Not Found", { status: 404 });
   }
 
   if (retrievedLink?.userId) {
-    const referer = req.headers.get("referer");
-    console.log(">>> Referer", referer);
-
     const userAgent = req.headers.get("user-agent");
     const parser = new UAParser(userAgent!);
     const userAgentDetails = parser.getResult();
@@ -69,9 +61,26 @@ export async function GET(req: Request) {
     const ipLocation = await fetch(`https://ipapi.co/${ip}/json/`).then((res) =>
       res.json(),
     );
-    console.log("User is logged in", retrievedLink.userId);
-    // TODO: Add user tracking
-    console.log(">>> User tracking");
+
+    if (
+      retrievedLink.disableLinkAfterClicks !== 0 &&
+      (await prisma.linkVisit.count({
+        where: {
+          linkId: retrievedLink.id,
+        },
+      })) >= retrievedLink.disableLinkAfterClicks!
+    ) {
+      await prisma.link.update({
+        where: {
+          id: retrievedLink.id,
+        },
+        data: {
+          disabled: true,
+        },
+      });
+      return new Response("Not Found", { status: 404 });
+    }
+
     await prisma.linkVisit.create({
       data: {
         linkId: retrievedLink.id,
