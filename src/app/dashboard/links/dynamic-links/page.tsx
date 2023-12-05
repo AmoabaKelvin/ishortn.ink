78815ceda64bf5e59976fc9769c7abd5d7aeb96d
AmoabaKelvin <kel.amoaba@gmail.com>
2023-12-05 19:46:54 +0000

    Add dynamic link creation page

---
+++ src/app/dashboard/links/dynamic-links/page.tsx
@@ -0,0 +1,11 @@
+import DynamicLinksForm from "@/components/forms/dashboard/links/dynamic-links-form";
+
+const DynamicLinkCreationPage = () => {
+  return (
+    <section>
+      <DynamicLinksForm />
+    </section>
+  );
+};
+
+export default DynamicLinkCreationPage;
