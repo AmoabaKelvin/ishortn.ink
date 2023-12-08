02075b02ca1597605a41206593a425d8f53df1e8
AmoabaKelvin <kel.amoaba@gmail.com>
2023-12-08 20:06:31 +0000

    Add CreateDynamicLinkProjectPage component

---
+++ src/app/dashboard/links/dynamic/project/create/page.tsx
@@ -0,0 +1,7 @@
+import DynamicLinksForm from "@/components/forms/dashboard/links/dynamic-links-project-form";
+
+const CreateDynamicLinkProjectPage = () => {
+  return <DynamicLinksForm />;
+};
+
+export default CreateDynamicLinkProjectPage;
