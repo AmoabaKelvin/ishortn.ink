fe6b0a0864cf209486df240207733cd15aaceaa3
AmoabaKelvin <kel.amoaba@gmail.com>
2023-12-09 12:51:54 +0000

    Refactor dynamic link resolution and redirection logic
    Do not show metadata when the request is made to the root subdomain as there are no associated metadata configuration

--- src/app/application/[software]/page.tsx
+++ src/app/application/[software]/page.tsx
@@ -1,34 +1,7 @@
 import prisma from "@/db";
-import { Metadata, ResolvingMetadata } from "next";
 import { headers } from "next/headers";
 import { notFound, redirect } from "next/navigation";
-type Props = {
-  params: { software: string };
-};
 
-export async function generateMetadata(
-  { params }: Props,
-  parent: ResolvingMetadata,
-): Promise<Metadata> {
-  const subdomain = params.software;
-
-  const dynamicLink = await prisma.dynamicLinkChildLink.findFirst({
-    where: {
-      shortLink: subdomain,
-    },
-  });
-
-  // get the metadata added by the user and merge it with the parent metadata
-  return {
-    title: `${dynamicLink?.metaDataTitle}` || "Link sharing powered by ishortn",
-    description:
-      `${dynamicLink?.metaDataDescription}` ||
-      "Link sharing powered by ishortn",
-    openGraph: {
-      images: [dynamicLink?.metaDataImageUrl as string],
-    },
-  };
-}
 const RedirectionPage = async ({
   params,
 }: {
@@ -36,12 +9,9 @@ const RedirectionPage = async ({
 }) => {
   const subdomain = params.software;
 
-  const resolvedDynamicLink = await prisma.dynamicLinkChildLink.findFirst({
+  const resolvedDynamicLink = await prisma.dynamicLink.findFirst({
     where: {
-      shortLink: subdomain,
-    },
-    include: {
-      dynamicLink: true,
+      subdomain: subdomain,
     },
   });
 
@@ -49,26 +19,24 @@ const RedirectionPage = async ({
     notFound();
   }
 
-  const dynamicLink = resolvedDynamicLink.dynamicLink;
-
   const incomingHeaders = headers();
   const userAgent = incomingHeaders.get("user-agent");
 
   if (userAgent?.includes("iPhone") || userAgent?.includes("iPad")) {
     redirect(
-      dynamicLink?.appStoreUrl
-        ? dynamicLink.appStoreUrl
-        : `https://apps.apple.com/app/id${dynamicLink?.iosBundleId}`,
+      resolvedDynamicLink?.appStoreUrl
+        ? resolvedDynamicLink.appStoreUrl
+        : `https://apps.apple.com/app/id${resolvedDynamicLink?.iosBundleId}`,
     );
   } else if (userAgent?.includes("Android")) {
     redirect(
-      dynamicLink?.playStoreUrl
-        ? dynamicLink.playStoreUrl
-        : `https://play.google.com/store/apps/details?id=${dynamicLink?.androidPackageName}`,
+      resolvedDynamicLink?.playStoreUrl
+        ? resolvedDynamicLink.playStoreUrl
+        : `https://play.google.com/store/apps/details?id=${resolvedDynamicLink?.androidPackageName}`,
     );
   }
 
-  redirect(resolvedDynamicLink.link);
+  return null;
 };
 
 export default RedirectionPage;
