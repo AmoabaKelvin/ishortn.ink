e90d9eff608be6854deb12bb3ba7f9500a64c2f8
AmoabaKelvin <kel.amoaba@gmail.com>
2023-12-09 12:52:27 +0000

    dynamic link path resolution page

---
+++ src/app/application/[software]/[path]/page.tsx
@@ -0,0 +1,91 @@
+import prisma from "@/db";
+import { Metadata, ResolvingMetadata } from "next";
+import { headers } from "next/headers";
+import { notFound, redirect } from "next/navigation";
+import PageRenderer from "./page-renderer";
+
+type Props = {
+  params: { software: string; path: string };
+};
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const subdomain = params.software;
+  const path = params.path;
+
+  const dynamicLink = await prisma.dynamicLink.findFirst({
+    where: {
+      subdomain: subdomain,
+    },
+    include: {
+      childLinks: {
+        where: {
+          shortLink: path,
+        },
+      },
+    },
+  });
+
+  const dynamicLinkChildLink = dynamicLink?.childLinks[0];
+
+  return {
+    title:
+      `${dynamicLinkChildLink?.metaDataTitle}` ||
+      "Link sharing powered by ishortn",
+    description:
+      `${dynamicLinkChildLink?.metaDataDescription}` ||
+      "Link sharing powered by ishortn",
+    openGraph: {
+      images: [dynamicLinkChildLink?.metaDataImageUrl as string],
+    },
+  };
+}
+
+const PathPage = async (params: { domain: string; slug: string }) => {
+  const subdomain = params.domain;
+
+  const dynamicLink = await prisma.dynamicLink.findFirst({
+    where: {
+      subdomain: subdomain,
+    },
+    include: {
+      childLinks: {
+        where: {
+          shortLink: params.slug,
+        },
+      },
+    },
+  });
+
+  if (!dynamicLink) {
+    notFound();
+  }
+
+  const incomingHeaders = headers();
+  const userAgent = incomingHeaders.get("user-agent");
+
+  if (userAgent?.includes("iPhone") || userAgent?.includes("iPad")) {
+    redirect(
+      dynamicLink?.appStoreUrl
+        ? dynamicLink.appStoreUrl
+        : `https://apps.apple.com/app/id${dynamicLink?.iosBundleId}`,
+    );
+  } else if (userAgent?.includes("Android")) {
+    redirect(
+      dynamicLink?.playStoreUrl
+        ? dynamicLink.playStoreUrl
+        : `https://play.google.com/store/apps/details?id=${dynamicLink?.androidPackageName}`,
+    );
+  }
+
+  return (
+    <PageRenderer
+      domain={dynamicLink.childLinks[0].link}
+      ogImage={dynamicLink.childLinks[0].metaDataImageUrl}
+    />
+  );
+};
+
+export default PathPage;
