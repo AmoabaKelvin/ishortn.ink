db3011a87ddd628c91e3c4b978dc974a8afef07a
AmoabaKelvin <kel.amoaba@gmail.com>
2023-12-10 11:28:30 +0000

    add pre-population of dynamic links project form fields

--- src/app/dashboard/links/dynamic/project/create/page.tsx
+++ src/app/dashboard/links/dynamic/project/create/page.tsx
@@ -1,6 +1,20 @@
 import DynamicLinksForm from "@/components/forms/dashboard/links/dynamic-links-project-form";
+import prisma from "@/db";
 
-const CreateDynamicLinkProjectPage = () => {
+const CreateDynamicLinkProjectPage = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) => {
+  if (searchParams.id) {
+    const project = await prisma.dynamicLink.findUnique({
+      where: { id: Number(searchParams.id) },
+    });
+
+    if (project) {
+      return <DynamicLinksForm initialValues={project} />;
+    }
+  }
   return <DynamicLinksForm />;
 };
 
