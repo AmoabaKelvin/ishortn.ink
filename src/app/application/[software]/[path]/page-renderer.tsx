efed9eea62aa78cbb9c2d09af7a2162abb961804
AmoabaKelvin <kel.amoaba@gmail.com>
2023-12-11 10:16:17 +0000

    fix: replace Image with img so we can render any href without doing any nextjs configuration

--- src/app/application/[software]/[path]/page-renderer.tsx
+++ src/app/application/[software]/[path]/page-renderer.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
@@ -18,13 +17,8 @@ const PageRenderer = ({
 
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen gap-4">
-      <Image
-        src={ogImage}
-        alt="og image"
-        className="rounded-md"
-        height={144}
-        width={144}
-      />
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img src={ogImage} alt="og image" className="rounded-md w-36 h-36" />
       <h1 className="text-3xl font-mazzardBold">Redirecting...</h1>
     </div>
   );
