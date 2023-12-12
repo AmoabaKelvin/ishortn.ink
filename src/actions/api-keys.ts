c868482bb1dfc6f37cef279093c86eaba07e220c
AmoabaKelvin <kel.amoaba@gmail.com>
2023-12-12 22:21:58 +0000

    add server actions for handling api-key related stuff

---
+++ src/actions/api-keys.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import { auth } from "@clerk/nextjs";
+import { Unkey } from "@unkey/api";
+import { revalidatePath } from "next/cache";
+
+const unkey = new Unkey({ token: process.env.UNKEY_TOKEN! });
+
+export const getUserAPIKeys = async () => {
+  const { userId } = auth();
+  const key = await unkey.apis.listKeys({
+    apiId: process.env.UNKEY_API_ID!,
+    ownerId: userId!,
+  });
+  return key;
+};
+
+export const createAPIKey = async () => {
+  const { userId } = auth();
+  const key = await unkey.keys.create({
+    apiId: process.env.UNKEY_API_ID!,
+    prefix: "ishortn",
+    ownerId: userId!,
+  });
+  // revalidatePath("/dashboard/settings");
+  return key;
+};
+
+export const revokeAPIKey = async (keyId: string) => {
+  const key = await unkey.keys.revoke({
+    keyId,
+  });
+  revalidatePath("/dashboard/settings");
+  return key;
+};
+
+export const revalidatePathForDashboard = () => {
+  revalidatePath("/dashboard/settings");
+};
