1b5d37ab1d5bb82e0b95f91f3edc40f9aa09b4ca
AmoabaKelvin <kel.amoaba@gmail.com>
2023-12-12 22:22:34 +0000

    Add settings page with API key functionality

---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,54 @@
+"use server";
+
+import { auth } from "@clerk/nextjs";
+import { Unkey } from "@unkey/api";
+import ApiKeyCard from "./api-key-card";
+import CreateAPIKey from "./create-key";
+
+const unkey = new Unkey({ token: process.env.UNKEY_TOKEN! });
+
+export async function create(ownerId: number) {
+  const key = await unkey.keys.create({
+    apiId: process.env.UNKEY_API_ID!,
+    ownerId: String(ownerId),
+    prefix: "ishortn",
+  });
+  return key;
+}
+
+export async function getUserKey(userID: string) {
+  const keys = await unkey.apis.listKeys({
+    ownerId: String(userID),
+    apiId: process.env.UNKEY_API_ID!,
+  });
+  return keys;
+}
+
+const SettingsPage = async () => {
+  const keys = await getUserKey(auth().userId!);
+
+  return (
+    <div>
+      <div className="max-w-3xl">
+        <h2 className="text-lg font-medium">API Keys</h2>
+        <p className="mt-2 mb-10 text-sm text-gray-600 dark:text-gray-400">
+          API Keys are used to authenticate requests to the API.
+        </p>
+
+        {keys.result?.total! > 0 ? (
+          <ApiKeyCard
+            start={keys.result?.keys[0].start!}
+            createdAt={keys.result?.keys[0].createdAt!}
+            keyID={keys.result?.keys[0].id!}
+          />
+        ) : (
+          <CreateAPIKey />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SettingsPage;
+
+// ishortn_3ZZhmQUSjRzvfZKcLx78mTSu
