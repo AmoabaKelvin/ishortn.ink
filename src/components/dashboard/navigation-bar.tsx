2b50ba9c6986fb6987a2334896c8d21503a8018a
AmoabaKelvin <kel.amoaba@gmail.com>
2023-12-14 16:32:30 +0000

    update discord invite link

--- src/components/dashboard/navigation-bar.tsx
+++ src/components/dashboard/navigation-bar.tsx
@@ -12,7 +12,7 @@ const NavigationBar = () => {
       </h2>
       <div className="flex items-center justify-between space-x-4">
         <div className="flex items-center gap-4">
-          <Link href="https://discord.gg/DE9xjVvk" target="_blank">
+          <Link href="https://discord.gg/S66ZvMzkU4" target="_blank">
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
               src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a69f118df70ad7828d4_icon_clyde_blurple_RGB.svg"
