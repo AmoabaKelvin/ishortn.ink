d4fde1af79fb5df927104e43e94bb2dda11fc0d5
AmoabaKelvin <kel.amoaba@gmail.com>
2023-12-18 22:39:49 +0000

    add dynamic-links to publicroutes

--- src/middleware.ts
+++ src/middleware.ts
@@ -81,6 +81,7 @@ export default async function middleware(
       "/",
       "/api/links",
       "/api/domains",
+      "/api/dynamic-links",
       "/apple-app-site-association",
       "/.well-known/assetlinks.json",
       "/:shortenedLink",
