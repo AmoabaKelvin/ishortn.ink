4eab090c54541fd80c2c793a0b9aa5c07fc3aa5f
AmoabaKelvin <kel.amoaba@gmail.com>
2024-01-07 10:26:58 +0000

    Delete unused files and code

--- src/config/schemas/link.ts
+++
@@ -1,8 +0,0 @@
-import * as zod from "zod";
-
-export const linkSchema = zod.object({
-  url: zod.string().url(),
-  alias: zod.string().max(10).optional(),
-});
-
-export type Link = zod.infer<typeof linkSchema>;
