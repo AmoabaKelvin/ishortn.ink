768c2827ca5d0a7b4a7fd0032203a53bcc7f85ea
AmoabaKelvin <kel.amoaba@gmail.com>
2024-01-27 10:40:17 +0000

    Add env module with configuration settings

---
+++ src/env.mjs
@@ -0,0 +1,36 @@
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.string().url(),
+    WEBHOOK_SECRET: z.string().min(1),
+    UNKEY_API_ID: z.string().min(1),
+    UNKEY_TOKEN: z.string().min(1),
+  },
+
+  clientPrefix: "NEXT_PUBLIC_",
+
+  client: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: z.string().min(1),
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: z.string().min(1),
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: z.string().min(1),
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: z.string().min(1),
+  },
+
+  runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    WEBHOOK_SECRET: process.env.WEBHOOK_SECRET,
+    UNKEY_API_ID: process.env.UNKEY_API_ID,
+    UNKEY_TOKEN: process.env.UNKEY_TOKEN,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL:
+      process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL,
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL:
+      process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL,
+  },
+});
