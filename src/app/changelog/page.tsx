84ac730d5d4b085632d4b3f8525ea9f6a6461ef4
Kelvin Amoaba <97001695+AmoabaKelvin@users.noreply.github.com>
2024-02-17 08:22:53 +0000

    Changelog page (#82)
    * install `gray-matter` and `changelog`
    
    * Add @tailwindcss/typography plugin
    
    * Add /changelog route to publicRoutes
    
    * feat: add changelog page
    
    * 📃 fresh changelogs
    
    * format with prettier

---
+++ src/app/changelog/page.tsx
@@ -0,0 +1,91 @@
+import { Button } from "@/components/ui/button";
+import { currentUser } from "@clerk/nextjs";
+import fs from "fs";
+import { GeistMono } from "geist/font/mono";
+import matter from "gray-matter";
+import Link from "next/link";
+import Markdown from "react-markdown";
+
+const getChangeLogsFromDirectory = () => {
+  const files = fs.readdirSync(process.cwd() + "/src/content/changelog");
+  const markdownFiles = files.filter((file) => file.endsWith(".md"));
+
+  const parsedFiles = markdownFiles.map((file) => {
+    return {
+      slug: file.replace(".md", ""),
+      ...matter(fs.readFileSync(`src/content/changelog/${file}`, "utf-8")),
+    };
+  });
+
+  const sortedFiles = parsedFiles.sort((a, b) => {
+    if (a.data.dateReleased < b.data.dateReleased) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+
+  return sortedFiles;
+};
+
+const ChangeLogPage = async () => {
+  const user = await currentUser();
+  const changeLogs = getChangeLogsFromDirectory();
+
+  return (
+    <>
+      <nav className="flex items-center justify-between max-w-5xl mx-auto my-8">
+        <Link className={`text-2xl font-bold ${GeistMono.className}`} href="/">
+          iShortn
+        </Link>
+        <Button asChild>
+          <Link href={user ? "/dashboard" : "/auth/sign-up"}>
+            {user ? "Dashboard" : "Sign Up"}
+          </Link>
+        </Button>
+      </nav>
+      <div
+        className={`flex flex-col justify-center max-w-5xl p-4 mx-auto border rounded-md ${GeistMono.className} shadow-sm`}
+      >
+        <div className="grid grid-cols-1 gap-3 md:grid-cols-12">
+          <div className="col-span-2"></div>
+          <div className="col-span-10">
+            <h1 className="text-3xl font-bold">ChangeLog</h1>
+            <p className="mt-2 mb-8 text-slate-500">
+              Here you can find all the changes that have been made to the
+              application ❤️
+            </p>
+          </div>
+
+          {changeLogs.map((changeLog) => {
+            return (
+              <>
+                <div className="col-span-2" key={changeLog.slug}>
+                  <p className="text-md text-slate-500">
+                    {new Date(changeLog.data.dateReleased).toLocaleDateString(
+                      "en-US",
+                      {
+                        year: "numeric",
+                        month: "short",
+                        day: "numeric",
+                      },
+                    )}
+                  </p>
+                </div>
+
+                <div className="col-span-10 mb-8">
+                  <h2 className="text-xl font-bold">{changeLog.data.title}</h2>
+                  <div className="mt-4 prose-sm prose text-slate-500 max-w-none prose-a:text-black prose-a:underline">
+                    <Markdown>{changeLog.content}</Markdown>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ChangeLogPage;
---
+++ src/content/changelog/redis-caching.md
@@ -0,0 +1,8 @@
+---
+title: Redis Caching for Faster Link Redirection
+dateReleased: "2024-01-31"
+---
+
+We have added Redis caching to iShortn to make link redirection faster. Redis is an in-memory data store that can be used as a cache. It is used to store the most frequently accessed data in memory, which makes it faster to access.
+
+Adding this layer saw a massive improvement in the speed of link redirection 🚀. This means that when a user clicks on a short link, the link will redirect to the destination URL faster than before. This will also save us some dollars 💸 and prolong our free-tier usage of some services 😁
