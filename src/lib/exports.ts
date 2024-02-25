e38608e9d221231c6e3abd4c541f315bef2634b4
kennyAnyi9 <kennyanyi@gmail.com>
2024-02-25 03:58:38 +0000

    chore: orgnize common exports in a single file

---
+++ src/lib/exports.ts
@@ -0,0 +1,30 @@
+import { CallToAction } from "@/components/landing/call-to-action";
+import { Container } from "@/components/landing/container";
+import { Faqs } from "@/components/landing/faqs";
+import { Footer } from "@/components/landing/footer";
+import { Header } from "@/components/landing/header";
+import { Hero } from "@/components/landing/hero";
+import Illustrations from "@/components/landing/illustration";
+import { PrimaryFeatures } from "@/components/landing/primary-features";
+import { TryOutNow } from "@/components/landing/try-out-now";
+import analyticsScreenShot from "../../public/images/screenshots/analytics.png";
+
+import linkShorteningScreenShot from "../../public/images/screenshots/create-link.png";
+import dynamicLinksScreenShot from "../../public/images/screenshots/dynamic-links-create.png";
+import screenshotHomeScreen from "../../public/images/screenshots/home-screen.png";
+
+export {
+  Container,
+  Hero,
+  TryOutNow,
+  Footer,
+  Header,
+  Faqs,
+  CallToAction,
+  PrimaryFeatures,
+  Illustrations,
+  analyticsScreenShot,
+  linkShorteningScreenShot,
+  dynamicLinksScreenShot,
+  screenshotHomeScreen,
+};
