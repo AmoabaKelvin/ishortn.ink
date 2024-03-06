bd46b13b0fb1a2a25191d4583ecf91c78429d5d8
kennyAnyi9 <kennyanyi@gmail.com>
2024-03-06 18:15:20 +0000

    style: remove huge margin from mobile view

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -84,11 +84,15 @@ function MobileNavigation() {
             <MobileNavLink href="#features">Features</MobileNavLink>
 
             <hr className="m-2 border-slate-300/40" />
-            <MobileNavLink href="/">Github</MobileNavLink>
+            <MobileNavLink href="https://github.com/AmoabaKelvin/ishortn.ink">
+              Github
+            </MobileNavLink>
             <hr className="m-2 border-slate-300/40" />
-            <MobileNavLink href="/">Twitter</MobileNavLink>
+
             <hr className="m-2 border-slate-300/40" />
-            <MobileNavLink href="/">Discord</MobileNavLink>
+            <MobileNavLink href="https://discord.com/invite/S66ZvMzkU4">
+              Discord
+            </MobileNavLink>
           </Popover.Panel>
         </Transition.Child>
       </Transition.Root>
@@ -97,6 +101,12 @@ function MobileNavigation() {
 }
 
 export function Header() {
+  const handleScrollingToElementHref = (href: string) => {
+    const element = document.querySelector(href);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
     <header className="py-5 border-b border-opacity-10 border-slate-900">
       <Container>
@@ -118,7 +128,11 @@ export function Header() {
             </div>
           </div>
           <div className="flex items-center gap-x-5 ">
-            <Link href="/">
+            <Link
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/AmoabaKelvin/ishortn.ink"
+            >
               <svg
                 viewBox="0 0 438.549 438.549"
                 className="h-5 w-5 hidden md:block"
@@ -129,7 +143,8 @@ export function Header() {
                 ></path>
               </svg>
             </Link>
-            <Link href="/">
+            {/* commented out the twitter icon */}
+            {/* <Link href="/">
               <svg
                 className="h-4 w-4 fill-current hidden md:block"
                 height="23"
@@ -139,8 +154,12 @@ export function Header() {
               >
                 <path d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z"></path>
               </svg>
-            </Link>
-            <Link href="/">
+            </Link> */}
+            <Link
+              target="_blank"
+              rel="noreferrer"
+              href="https://discord.com/invite/S66ZvMzkU4"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="20"
