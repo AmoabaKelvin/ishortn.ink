cac9239cf06190a378af64ffe6ad57c9f79d7846
AmoabaKelvin <kel.amoaba@gmail.com>
2024-03-28 06:41:05 +0000

    add link-related functions

---
+++ src/lib/links/index.ts
@@ -0,0 +1,78 @@
+import { Prisma } from "@prisma/client";
+import { customAlphabet } from "nanoid";
+
+import prisma from "@/db";
+
+import { addLinkToCache, retrieveLinkFromCache } from "../cache";
+
+const checkForAliasCollision = async (alias: string) => {
+  const existingLink = await prisma.link.findFirst({
+    where: {
+      alias,
+    },
+  });
+
+  return !!existingLink;
+};
+
+export const generateShortLink = async () => {
+  const alphabet =
+    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  const nanoid = customAlphabet(alphabet, 7);
+
+  const shortLink = nanoid();
+
+  while (true) {
+    if (await checkForAliasCollision(shortLink)) {
+      return nanoid();
+    }
+
+    return shortLink;
+  }
+};
+
+export const hasLinkExceededSpecifiedClicks = async (
+  retrievedLink: Prisma.LinkGetPayload<{}>,
+) => {
+  if (retrievedLink.disableLinkAfterClicks) {
+    const linkVisits = await prisma.linkVisit.findMany({
+      where: {
+        linkId: retrievedLink.id,
+      },
+    });
+
+    return linkVisits.length >= retrievedLink.disableLinkAfterClicks;
+  }
+
+  return false;
+};
+
+export const hasLinkExceededSpecifiedDate = async (
+  retrievedLink: Prisma.LinkGetPayload<{}>,
+): Promise<boolean> => {
+  if (retrievedLink.disableLinkAfterDate) {
+    const currentDate = new Date();
+    const linkDate = new Date(retrievedLink.disableLinkAfterDate);
+    return currentDate >= linkDate;
+  }
+
+  return false;
+};
+
+export const retrieveLinkFromCacheOrDatabase = async (alias: string) => {
+  const link = await retrieveLinkFromCache(alias);
+
+  if (link) return link;
+
+  const retrievedLink = await prisma.link.findFirst({
+    where: {
+      alias,
+    },
+  });
+
+  if (retrievedLink) {
+    await addLinkToCache(retrievedLink);
+  }
+
+  return retrievedLink;
+};
