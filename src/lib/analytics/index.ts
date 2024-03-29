ef79caf07445ed0d3fd93fb06497c8f7b3cbdfe6
AmoabaKelvin <kel.amoaba@gmail.com>
2024-03-29 08:20:35 +0000

    feat: when in production, use geolocation api to retrieve geolocation details

--- src/lib/analytics/index.ts
+++ src/lib/analytics/index.ts
@@ -1,18 +1,29 @@
 import { HonoRequest } from "hono";
-import { NextRequest } from "next/server";
 import UAParser from "ua-parser-js";
 
-const DEFAULT_GEOLOCATION_FOR_LOCALHOST = {
+import { env } from "@/env.mjs";
+
+const DEFAULT_GEOLOCATION_DETAILS_FOR_LOCALHOST = {
   city: "localhost",
   country: "localhost",
 };
 
-const getGeolocationDetails = async (req: HonoRequest) => {
-  const nextRequest = new NextRequest(req.raw);
+const GEOLOCATION_API_URL = `https://api.findip.net/ipHere/?token=${env.GEOLOCATION_API_KEY}`;
 
+const getGeolocationDetailsFromAPI = async (ip: string) => {
+  const response = await fetch(GEOLOCATION_API_URL.replace("ipHere", ip));
+  const data: GeolocationAPIResponseType = await response.json();
+
+  return {
+    city: data.city.names.en,
+    country: data.country.names.en,
+  };
+};
+
+const getGeolocationDetails = async (ip: string) => {
   const geolocationDetails = process.env.VERCEL
-    ? nextRequest.geo
-    : DEFAULT_GEOLOCATION_FOR_LOCALHOST;
+    ? await getGeolocationDetailsFromAPI(ip)
+    : DEFAULT_GEOLOCATION_DETAILS_FOR_LOCALHOST;
 
   console.log("geolocationDetails", geolocationDetails);
 
@@ -66,11 +77,75 @@ const getUserIP = (req: HonoRequest) => {
 export const getDeviceAndGeolocationDetails = async (req: HonoRequest) => {
   const [deviceDetails, geolocationDetails] = await Promise.all([
     getRequestingDeviceDetails(req),
-    getGeolocationDetails(req),
+    getGeolocationDetails(getUserIP(req)!),
   ]);
 
   return {
     ...deviceDetails,
     ...geolocationDetails,
+  };
+};
+
+type GeolocationAPIResponseType = {
+  city: {
+    geoname_id: number;
+    names: {
+      en: string;
+    };
+  };
+  continent: {
+    code: string;
+    geoname_id: number;
+    names: {
+      de: string;
+      en: string;
+      es: string;
+      fa: string;
+      fr: string;
+      ja: string;
+      ko: string;
+      "pt-BR": string;
+      ru: string;
+      "zh-CN": string;
+    };
+  };
+  country: {
+    geoname_id: number;
+    is_in_european_union: boolean;
+    iso_code: string;
+    names: {
+      de: string;
+      en: string;
+      es: string;
+      fa: string;
+      fr: string;
+      ja: string;
+      ko: string;
+      "pt-BR": string;
+      ru: string;
+      "zh-CN": string;
+    };
+  };
+  location: {
+    latitude: number;
+    longitude: number;
+    time_zone: string;
+    weather_code: string;
+  };
+  subdivisions: Array<{
+    geoname_id: number;
+    iso_code?: string;
+    names: {
+      en: string;
+      ko?: string;
+    };
+  }>;
+  traits: {
+    autonomous_system_number: number;
+    autonomous_system_organization: string;
+    connection_type: string;
+    isp: string;
+    organization: string;
+    user_type: string;
   };
 };
