601dddb25054f957edeab88d9e7d1f402360b541
Kelvin Amoaba <97001695+AmoabaKelvin@users.noreply.github.com>
2024-03-31 07:20:14 +0000

    sunsetting upstash redis (#109)

--- src/lib/utils/cache/index.ts
+++ src/lib/utils/cache/index.ts
@@ -1,21 +1,16 @@
 "use server";
 
 import { Prisma } from "@prisma/client";
-import { Redis } from "@upstash/redis";
+import Redis from "ioredis";
 
 import { env } from "@/env.mjs";
 
-const redis = new Redis({
-  url: env.UPSTASH_URL,
-  token: env.UPSTASH_TOKEN,
-});
+const redis = new Redis(env.REDIS_URL);
 
 export const retrieveLinkFromCache = async (alias: string) => {
   try {
-    const retrievedLink = (await redis.hgetall(
-      alias,
-    )) as Prisma.LinkGetPayload<{}>;
-    return retrievedLink;
+    const retrievedLink = await redis.hgetall(alias);
+    return convertToPrismaLink(retrievedLink);
   } catch (error) {
     return null;
   }
@@ -35,4 +30,22 @@ export const deleteLinkFromCache = async (alias: string) => {
   } catch (error) {
     return null;
   }
+};
+
+const convertToPrismaLink = (link: Record<string, string>) => {
+  return {
+    id: Number(link.id),
+    url: link.url,
+    alias: link.alias,
+    userId: link.userId || null,
+    createdAt: new Date(link.createdAt),
+    disableLinkAfterClicks: link.disableLinkAfterClicks
+      ? Number(link.disableLinkAfterClicks)
+      : null,
+    disableLinkAfterDate: link.disableLinkAfterDate
+      ? new Date(link.disableLinkAfterDate)
+      : null,
+    disabled: link.disabled === "true",
+    publicStats: link.publicStats === "true",
+  };
 };
