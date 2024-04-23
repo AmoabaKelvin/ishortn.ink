2425412b612adc7e3296132c6e4dae83d1ed3842
AmoabaKelvin <kel.amoaba@gmail.com>
2024-04-23 18:11:29 +0000

    fix: do not redirect for domains that start with _

--- src/middleware.ts
+++ src/middleware.ts
@@ -91,7 +91,8 @@ export default async function middleware(
   if (
     url.pathname.split("/").length == 2 &&
     !domainsThatShouldNotRedirect.includes(paths[1]) &&
-    !paths[1].includes(".")
+    !paths[1].includes(".") &&
+    !paths[1].startsWith("_")
   ) {
     console.log("Performing redirection in middleware");
     const response = await fetch(
