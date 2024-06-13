086f2390936abde3022e82977a053aeb2349b593
AmoabaKelvin <kel.amoaba@gmail.com>
2024-06-13 20:46:19 +0000

    refactor: move `aggregateVisits` to analytics utils

--- src/lib/core/analytics/index.ts
+++ src/lib/core/analytics/index.ts
@@ -3,6 +3,7 @@ import UAParser from "ua-parser-js";
 import { env } from "@/env";
 import { LOCAL_DEVELOPMENT_GEOLOCATION_DATA } from "@/lib/constants";
 
+import type { RouterOutputs } from "@/trpc/shared";
 import type { GeolocationAPIResponseType } from "./types";
 
 const getGeolocationDetailsFromAPI = async (ip: string) => {
@@ -72,4 +73,80 @@ export const retrieveDeviceAndGeolocationData = async (headers: Headers) => {
     ...deviceDetails,
     ...geolocationDetails,
   };
+};
+
+export const aggregateVisits = (linkVisits: RouterOutputs["link"]["linkVisits"]) => {
+  const clicksPerDate: Record<string, number> = {};
+  const clicksPerCity: Record<string, number> = {};
+  const clicksPerCountry: Record<string, number> = {};
+  const clicksPerDevice: Record<string, number> = {};
+  const clicksPerOS: Record<string, number> = {};
+  const clicksPerBrowser: Record<string, number> = {};
+  const clicksPerModel: Record<string, number> = {};
+
+  linkVisits.forEach((visit: RouterOutputs["link"]["linkVisits"][number]) => {
+    const date = new Date(visit.createdAt!).toLocaleDateString();
+
+    if (!clicksPerDate[date]) {
+      clicksPerDate[date] = 0;
+    }
+
+    clicksPerDate[date] += 1;
+
+    if (!clicksPerCountry[visit.country!]) {
+      clicksPerCountry[visit.country!] = 0;
+    }
+
+    clicksPerCountry[visit.country!] += 1;
+
+    if (!clicksPerCity[visit.city!]) {
+      clicksPerCity[visit.city!] = 0;
+    }
+
+    clicksPerCity[visit.city!] += 1;
+
+    if (!clicksPerDevice[visit.device!]) {
+      clicksPerDevice[visit.device!] = 0;
+    }
+
+    clicksPerDevice[visit.device!] += 1;
+
+    if (!clicksPerOS[visit.os!]) {
+      clicksPerOS[visit.os!] = 0;
+    }
+
+    clicksPerOS[visit.os!] += 1;
+
+    if (!clicksPerBrowser[visit.browser!]) {
+      clicksPerBrowser[visit.browser!] = 0;
+    }
+
+    clicksPerBrowser[visit.browser!] += 1;
+
+    if (!clicksPerModel[visit.model!]) {
+      clicksPerModel[visit.model!] = 0;
+    }
+
+    clicksPerModel[visit.model!] += 1;
+
+    return {
+      clicksPerDate,
+      clicksPerCountry,
+      clicksPerCity,
+      clicksPerDevice,
+      clicksPerOS,
+      clicksPerBrowser,
+      clicksPerModel,
+    };
+  });
+
+  return {
+    clicksPerDate,
+    clicksPerCountry,
+    clicksPerCity,
+    clicksPerDevice,
+    clicksPerOS,
+    clicksPerBrowser,
+    clicksPerModel,
+  } as const;
 };
