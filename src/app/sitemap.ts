3a7540ea7f0db49e8cae6fa981077d3bbd71d38b
AmoabaKelvin <kel.amoaba@gmail.com>
2024-06-15 16:00:22 +0000

    refactor: update sitemap generation to include additional routes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,12 +1,22 @@
-import { type MetadataRoute } from "next";
+import type { MetadataRoute } from "next";
 
 import { absoluteUrl } from "@/lib/utils";
 
+const routes = [
+  "",
+  "/analytics",
+  "/dashboard",
+  "/dashboard/analytics",
+  "/dashboard/tokens",
+  "/dashboard/links/new",
+  "/dashboard/billing",
+];
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const routes = ["", "/dashboard", "/dashboard/billing"].map((route) => ({
+  const sitemapRoutes = routes.map((route) => ({
     url: absoluteUrl(route),
     lastModified: new Date().toISOString(),
   }));
 
-  return [...routes];
+  return [...sitemapRoutes];
 }
