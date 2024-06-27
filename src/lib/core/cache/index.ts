20900b7871ebb0078c49a05daed07c26835e4b7f
AmoabaKelvin <kel.amoaba@gmail.com>
2024-06-27 04:32:38 +0000

    feat: Add Redis caching for link retrieval and storage

---
+++ src/lib/core/cache/index.ts
@@ -0,0 +1,59 @@
+import { Redis } from "ioredis";
+
+import { env } from "@/env";
+
+import type { WithoutNull } from "@/server/api/trpc";
+import type { Link } from "@/server/db/schema";
+
+type NonNullableLink = WithoutNull<Link>;
+
+export class Cache {
+  private redis: Redis;
+
+  constructor() {
+    this.redis = new Redis(env.REDIS_URL);
+  }
+
+  async get(key: string): Promise<Link | null> {
+    try {
+      const retrievedLink = await this.redis.hgetall(key);
+      return convertToLink(retrievedLink);
+    } catch (error) {
+      return null;
+    }
+  }
+
+  async set(link: NonNullableLink): Promise<boolean> {
+    try {
+      await this.redis.hset(link.alias, link);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  async delete(key: string): Promise<boolean> {
+    try {
+      await this.redis.del(key);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+}
+
+function convertToLink(link: Record<string, string>): Link | null {
+  return {
+    id: Number(link.id),
+    url: link.url!,
+    alias: link.alias!,
+    userId: link.userId!,
+    createdAt: new Date(link.createdAt!),
+    disableLinkAfterClicks: link.disableLinkAfterClicks
+      ? Number(link.disableLinkAfterClicks)
+      : null,
+    disableLinkAfterDate: link.disableLinkAfterDate ? new Date(link.disableLinkAfterDate) : null,
+    disabled: link.disabled === "true",
+    publicStats: link.publicStats === "true",
+  };
+}
