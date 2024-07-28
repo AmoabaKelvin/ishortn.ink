1994fe694cff8d048e156f4dbce02c3bfc5f8d0c
AmoabaKelvin <kel.amoaba@gmail.com>
2024-07-28 13:32:13 +0000

    remove logging

--- src/app/(main)/[linkAlias]/page.tsx
+++ src/app/(main)/[linkAlias]/page.tsx
@@ -22,8 +22,6 @@ const LinkRedirectionPage = async ({ params }: LinkRedirectionPageProps) => {
     domain = "ishortn.ink";
   }
 
-  console.log("Domain", domain);
-
   const link = await api.link.retrieveOriginalUrl.query({
     alias: params.linkAlias,
     domain: domain.replace("http://", "").replace("https://", "").replace("www.", ""),
