e30a974724f41f8a11fa1da3e4fe7cbc15550f35
AmoabaKelvin <kel.amoaba@gmail.com>
2024-11-15 22:57:47 +0000

    style(dashboard-nav): adjust layout and improve link formatting

--- src/app/(main)/dashboard/_components/dashboard-nav.tsx
+++ src/app/(main)/dashboard/_components/dashboard-nav.tsx
@@ -8,7 +8,7 @@ const DashboardNav = () => {
         <Link href="/" className="text-blue-600 dark:text-blue-500">
           ishortn.ink
         </Link>{" "}
-        <span className="ml-2 hidden sm:block">/ Dashboard</span>
+        <span className="hidden ml-2 sm:block">/ Dashboard</span>
       </h2>
       <div className="flex items-center justify-between space-x-4">
         <div className="flex items-center gap-4">
@@ -27,7 +27,10 @@ const DashboardNav = () => {
               alt="Discord"
             />
           </Link>
-          <Link href="https://github.com/AmoabaKelvin/ishortn.ink" target="_blank">
+          <Link
+            href="https://github.com/AmoabaKelvin/ishortn.ink"
+            target="_blank"
+          >
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/600px-GitHub_Invertocat_Logo.svg.png"
@@ -39,7 +42,6 @@ const DashboardNav = () => {
         </div>
         <UserButton />
       </div>
-      {/* <LogoutButton /> */}
     </div>
   );
 };
