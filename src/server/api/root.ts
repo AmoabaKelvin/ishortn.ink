5b95e1693bbb5a0eb15016b5b70143771a6b3e08
AmoabaKelvin <kel.amoaba@gmail.com>
2024-11-15 22:53:45 +0000

    feat(api): add site settings router to the main application router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -3,6 +3,7 @@ import { customDomainRouter } from "./routers/domains/domains.procedure";
 import { lemonsqueezyRouter } from "./routers/lemonsqueezy/lemonsqueezy.procedure";
 import { linkRouter } from "./routers/link/link.procedure";
 import { qrCodeRouter } from "./routers/qrcode/qrcode.procedure";
+import { siteSettingsRouter } from "./routers/settings/settings.procedure";
 import { subscriptionsRouter } from "./routers/subscriptions/subscriptions.procedure";
 import { tokenRouter } from "./routers/token/token.procedure";
 import { createTRPCRouter } from "./trpc";
@@ -15,6 +16,7 @@ export const appRouter = createTRPCRouter({
   qrCode: qrCodeRouter,
   customDomain: customDomainRouter,
   ai: aiRouter,
+  siteSettings: siteSettingsRouter,
 });
 
 export type AppRouter = typeof appRouter;
