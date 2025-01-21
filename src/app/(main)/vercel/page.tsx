95df719ca5d9dc217d94dff8f78dd4c26c93976d
AmoabaKelvin <kel.amoaba@gmail.com>
2025-01-21 17:45:49 +0000

    feat: get country and city information directly

--- src/app/(main)/vercel/page.tsx
+++ src/app/(main)/vercel/page.tsx
@@ -1,11 +1,16 @@
-import { api } from "@/trpc/server";
+import { headers } from "next/headers";
 
 export default function VercelPage() {
-  const responseFromCheckingVercelHeaders = api.link.checkVercelHeaders.query();
+  const incomingHeaders = headers();
+  const countryInformation = incomingHeaders.get("X-Vercel-IP-Country ");
+  const cityInformation = incomingHeaders.get("X-Vercel-IP-City ");
+  // const responseFromCheckingVercelHeaders = api.link.checkVercelHeaders.query();
   return (
     <div>
       <h1>Vercel</h1>
-      <p>{JSON.stringify(responseFromCheckingVercelHeaders)}</p>
+      {/* <p>{JSON.stringify(responseFromCheckingVercelHeaders)}</p> */}
+      <p>{countryInformation}</p>
+      <p>{cityInformation}</p>
     </div>
   );
 }
