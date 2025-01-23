ed402b3ee542fe93fd7fca2343d82218ccafdc75
AmoabaKelvin <kel.amoaba@gmail.com>
2025-01-23 08:43:05 +0000

    feat: resolve short links in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,67 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { geolocation } from "@vercel/functions";
-import { NextRequest } from "next/server";
+import { geolocation, ipAddress } from "@vercel/functions";
+import { NextRequest, NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
-export function userLocationMiddleware(request: NextRequest) {
+async function resolveLinkAndLogAnalytics(request: NextRequest) {
+  if (isProtectedRoute(request)) {
+    return; // Let Clerk handle protected routes
+  }
+
+  const { pathname, host } = new URL(request.url);
+
+  if (
+    pathname === "/" ||
+    pathname.startsWith("/api/") ||
+    pathname.startsWith("/dashboard") ||
+    pathname.startsWith("/_next/")
+  ) {
+    return NextResponse.next();
+  }
+
   const geo = geolocation(request);
-  console.log("Geo: ", geo);
-  // return NextResponse.redirect(new URL('/home', request.url))
+  const ip = ipAddress(request);
+  const userAgent = request.headers.get("user-agent");
+  const referer = request.headers.get("referer");
+
+  const response = await fetch(
+    encodeURI(
+      `${
+        request.url.split(pathname)[0]
+      }/api/link?domain=${host}&alias=${pathname}&country=${geo.country}&city=${
+        geo.city
+      }&ip=${ip}`
+    ),
+    {
+      headers: {
+        "user-agent": userAgent ?? "",
+        referer: referer ?? "",
+      },
+    }
+  );
+
+  if (!response.ok) {
+    return NextResponse.next();
+  }
+
+  const data = await response.json();
+
+  if (!data.url) {
+    return NextResponse.next();
+  }
+
+  return NextResponse.redirect(data.url);
 }
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
-  return userLocationMiddleware(req);
+  return resolveLinkAndLogAnalytics(req);
 });
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|favicon|^[^/]+$|.*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
