14c1c385f1da41fce6baf75f6c7980d65441e6a7
AmoabaKelvin <kel.amoaba@gmail.com>
2026-01-06 10:56:24 +0000

    refactor: rename router files to follow .procedure.ts convention
    Rename for consistency with other routers in the codebase:
    - tag.router.ts → tag.procedure.ts
    - utm-template.router.ts → utm-template.procedure.ts

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -7,11 +7,11 @@ import { linkRouter } from "./routers/link/link.procedure";
 import { qrCodeRouter } from "./routers/qrcode/qrcode.procedure";
 import { siteSettingsRouter } from "./routers/settings/settings.procedure";
 import { subscriptionsRouter } from "./routers/subscriptions/subscriptions.procedure";
-import { tagRouter } from "./routers/tag/tag.router";
+import { tagRouter } from "./routers/tag/tag.procedure";
 import { teamRouter } from "./routers/team/team.procedure";
 import { tokenRouter } from "./routers/token/token.procedure";
 import { userRouter } from "./routers/user/user.procedure";
-import { utmTemplateRouter } from "./routers/utm-template/utm-template.router";
+import { utmTemplateRouter } from "./routers/utm-template/utm-template.procedure";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
